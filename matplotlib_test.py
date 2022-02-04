2cd2413d9c66b0113e7bb199d50002ea0bf95fa3
hericklima22 <herick.lima77@gmail.com>
2022-02-03 22:17:24 -0300

    histograma

--- matplotlib_test.py
+++ matplotlib_test.py
@@ -27,23 +27,38 @@ from matplotlib import pyplot as plt
 
 # plt.show()
 
-#cria uma figura para 2 subplots (1 linha, 2 colunas)
-fig, ax = plt.subplots(1, 2, figsize = (10,4))
+# #cria uma figura para 2 subplots (1 linha, 2 colunas)
+# fig, ax = plt.subplots(1, 2, figsize = (10,4))
 
-#cria um plot em barra do nome vs nota no primeiro eixo
-ax[0].bar(x=df_students.Name, height=df_students.Grade, color='orange')
-ax[0].set_title("Notas")
-ax[0].set_xticklabels(df_students.Name, rotation=90)
-
-
-#cia um chart de torta dque passa o count no segundo eixo
-pass_counts = df_students['Pass'].value_counts()
-ax[1].pie(pass_counts, labels=pass_counts)
-ax[1].set_title('Passando notas')
-ax[1].legend(pass_counts.keys().tolist())
+# #cria um plot em barra do nome vs nota no primeiro eixo
+# ax[0].bar(x=df_students.Name, height=df_students.Grade, color='orange')
+# ax[0].set_title("Notas")
+# ax[0].set_xticklabels(df_students.Name, rotation=90)
 
 
-#adiciona um titulo para a figura
-fig.suptitle('Dados dos estudantes')
+# #cia um chart de torta dque passa o count no segundo eixo
+# pass_counts = df_students['Pass'].value_counts()
+# ax[1].pie(pass_counts, labels=pass_counts)
+# ax[1].set_title('Passando notas')
+# ax[1].legend(pass_counts.keys().tolist())
+
+
+# #adiciona um titulo para a figura
+# fig.suptitle('Dados dos estudantes')
+
+# fig.show()
+
+
+var_data = df_students['Grade']
+
+fig = plt.figure(figsize=(10,4))
+
+# plota o histograma
+plt.hist(var_data)
+
+#adiciona titulo e labels
+plt.title('Distribuicao de dados')
+plt.xlabel('Valor')
+plt.ylabel('Frequencia')
 
 fig.show()
